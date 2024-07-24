076ee27818776ce9ecaaccf25d9edd3121161e29
JinYunhwa <https://github.com/min3009/faq_crawling>
2024-07-24 16:04:59 +0900

    - feat : 모든 카테고리에서 데이터를 크롤링함

--- hyundai-crawler.py
+++ hyundai-crawler.py
@@ -7,15 +7,80 @@ import pandas as pd
 import time
 
 
-# 특정 요소가 있는지 확인하는 함수
-def check_element_exists(by, value):
+def check_last_button():
+    for i in range(1,8):
+        try:
+            num = 9-i
+            btn_css = f"#app > div.contant-area > section > div.l-container-body > div > div.l-contents-mid > section > div > div:nth-child(3) > div.pagenation.pagenation > div > ul > li:nth-child({num}) > button"
+            result = driver.find_element(By.CSS_SELECTOR, btn_css).text
+            print(result)
+            return  result
+        except:
+            continue
+    return False
+
+def check_element_exist(by, value):
     try:
-        driver.find_element(by, value)
+        # 특정 클래스가 존재하는지 확인
+        element = driver.find_element(by,value)
+        print("Element found")
         return True
     except NoSuchElementException:
+        print("Element not found")
         return False
-    
-def click_title(title_css_selector):
+
+def get_faq(last_page):
+    print_titles()
+    if last_page == 1:
+        return
+    elif last_page < 6:
+        for i in range(1, last_page):
+            num = i+1
+            click_page_num(num)
+            print_titles()
+        return
+    elif last_page == 6:
+        for _ in range(2):
+            driver.find_element(By.XPATH, next_button_xpath).click()
+            print_titles()
+        for i in range(4,last_page):
+            click_page_num(i)
+            print_titles()
+        return
+    elif last_page == 7:
+        for _ in range(3):
+            driver.find_element(By.XPATH, next_button_xpath).click()
+            print_titles()
+        for i in range(5,last_page):
+            click_page_num(i)
+            print_titles()
+        click_page_num(last_page)
+        print_titles()
+        return
+    elif last_page == 8:
+        for _ in range(3):
+            driver.find_element(By.XPATH, next_button_xpath).click()
+            print_titles()
+        for i in range(5,last_page):
+            click_page_num(i)
+            print_titles()
+            if i == 5:
+                click_page_num(i)
+                print_titles()
+        return
+    else:
+        for _ in range(7):
+            driver.find_element(By.XPATH, next_button_xpath).click()
+            print_titles()
+        for i in range(8,last_page-2):
+            click_page_num(5)
+            print_titles()
+        for i in range(6,8):
+            click_page_num(i)
+            print_titles()
+        return
+
+def click_element(title_css_selector):
     try:
         element = driver.find_element(By.CSS_SELECTOR, title_css_selector)
         element.click()
@@ -24,11 +89,20 @@ def click_title(title_css_selector):
         print(f"Error: {e}")
     return
 
+def click_page_num(num):
+    css_selector = f'#app > div.contant-area > section > div.l-container-body > div > div.l-contents-mid > section > div > div:nth-child(3) > div.pagenation.pagenation > div > ul > li:nth-child({num}) > button'
+    try:
+        driver.find_element(By.CSS_SELECTOR, css_selector).click()
+        time.sleep(0.1)
+    except:
+        return
+    return
+
 def print_content(content_css_selector):
     try:
         element = driver.find_element(By.CSS_SELECTOR, content_css_selector).text
         data.append(element)
-        print(element)
+        print(element)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
     except Exception as e:
         print(f"Error: {e}")
     return
@@ -44,19 +118,19 @@ def print_titles():
         print(title)
         num += 1
         title_css_selector = f"#app > div.contant-area > section > div.l-container-body > div > div.l-contents-mid > section > div > div:nth-child(3) > div.list-wrap > div:nth-child({num}) > button > div > span.list-content"
-        click_title(title_css_selector)
+        click_element(title_css_selector)
         print_content(content_css_selector)
-        click_title(title_css_selector)
+        click_element(title_css_selector)
     time.sleep(0.1)
     print("############ ")
     return
 
-def scroll_down():
+def scroll_move(scroll_len):
     # 현재 스크롤 좌표 추출
     current_scroll_position = driver.execute_script("return window.pageYOffset")
     print(f"Current scroll position: {current_scroll_position}")
     # 지정 좌표로 스크롤 이동
-    driver.execute_script(f"window.scrollTo(0, {current_scroll_position + 1150})")
+    driver.execute_script(f"window.scrollTo(0, {current_scroll_position + scroll_len})")
     # 스크롤 후 대기
     
 
@@ -66,7 +140,7 @@ data = []
 # Selenium 설정
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.set_window_size(960, 1080) 
-driver.set_window_position(760,0)
+driver.set_window_position(660,0)
 
 # URL 설정
 url = "https://www.hyundai.com/kr/ko/e/customer/center/faq"
@@ -78,36 +152,35 @@ next_button_xpath = '//*[@id="app"]/div[3]/section/div[2]/div/div[2]/section/div
 # 페이지 로드 대기
 driver.implicitly_wait(5)
 
+scroll_move(1150)
+time.sleep(2)
 
-scroll_down()
-time.sleep(3)
-print_titles()
+last_page_num = int(check_last_button())
+get_faq(last_page_num)
 
-for _ in range(3):
-    driver.find_element(By.XPATH, next_button_xpath).click()
-    time.sleep(0.1)
-    print_titles()
+#원래 range안의 값은 2
+for i in range(2,10):
+    num = i
+    cate_css = f"#app > div.contant-area > section > div.l-container-body > div > div.l-contents-mid > section > div > div:nth-child(1) > div.tab-menu > ul > li:nth-child({num}) > button"
 
-#xpath 3회 이후, 5, 5, 6, 7을 셀렉터로 클릭해야함. 
-css_selector = '#app > div.contant-area > section > div.l-container-body > div > div.l-contents-mid > section > div > div:nth-child(3) > div.pagenation.pagenation > div > ul > li:nth-child(5) > button'
-driver.find_element(By.CSS_SELECTOR, css_selector).click()
-time.sleep(0.1)
-print_titles()
+    scroll_move(-300)
+    time.sleep(1)
+    try:
+        driver.find_element(By.CSS_SELECTOR, cate_css).click()
+    except:
+        print("요소가 존재하지 않습니다.")
+    scroll_move(300)
+    time.sleep(0.5)
+    
+    # 페이지를 제일 처음으로 이동
+    navi_css = '#app > div.contant-area > section > div.l-container-body > div > div.l-contents-mid > section > div > div:nth-child(3) > div.pagenation.pagenation > button.btn.btn-prevall.ative'
+    try:
+        driver.find_element(By.CSS_SELECTOR, navi_css).click()
+    except:
+        print("페이지가 첫페이지 입니다.")
 
-css_selector = '#app > div.contant-area > section > div.l-container-body > div > div.l-contents-mid > section > div > div:nth-child(3) > div.pagenation.pagenation > div > ul > li:nth-child(5) > button'
-driver.find_element(By.CSS_SELECTOR, css_selector).click()
-time.sleep(0.1)
-print_titles()
-
-css_selector = '#app > div.contant-area > section > div.l-container-body > div > div.l-contents-mid > section > div > div:nth-child(3) > div.pagenation.pagenation > div > ul > li:nth-child(6) > button'
-driver.find_element(By.CSS_SELECTOR, css_selector).click()
-time.sleep(0.1)
-print_titles()
-
-css_selector = '#app > div.contant-area > section > div.l-container-body > div > div.l-contents-mid > section > div > div:nth-child(3) > div.pagenation.pagenation > div > ul > li:nth-child(7) > button'
-driver.find_element(By.CSS_SELECTOR, css_selector).click()
-time.sleep(0.1)
-print_titles()
+    last_page_num = int(check_last_button())
+    get_faq(last_page_num)
 
 # 데이터를 두 개의 열로 나누기
 # 예를 들어, 짝수 인덱스는 'Column1', 홀수 인덱스는 'Column2'로 나누기
@@ -119,6 +192,8 @@ df = pd.DataFrame({'질문': column1, '답변': column2})
 
 # CSV 파일로 저장
 df.to_csv('hyundai-output.csv', index=False)
+
+time.sleep(3000)
 
 # 브라우저 닫기
 driver.quit()
